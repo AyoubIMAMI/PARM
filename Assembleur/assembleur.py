f9aba83bbfd84dfae1b4256454826504a4956317
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2021-12-31 14:50:54 +0100

    Ajout du add sur SP

--- Assembleur/assembleur.py
+++ Assembleur/assembleur.py
@@ -81,6 +81,25 @@ def addsToBinary(instructionString):
     instructionList = getInstructionList(instructionString)
     registryKeyList = registryDic.keys()
 
+    if len(instructionList) < 3:
+        return instructionString
+    
+    if not "add" in instructionList[0]:
+        return instructionString
+
+    # 9.1.4.1 ADD (SP plus immediate) : Add Immediate to SP (p. 194)
+    # Checking for:  SP, #<offset>
+    if instructionList[1] == "sp" and  instructionList[2].startswith('#'):
+        imm7 = format(int(int(instructionList[2].replace('#', '')) / int(4)), 'b')
+        
+        if len(imm7) > 7:
+            raise MemoryError("imm7 is too big (> 7 bits)")
+        else:
+            imm7Size = len(imm7)
+            for i in range(0, 7 - imm7Size):
+                imm7 = '0' + imm7
+        return "101100000 " + imm7 
+
     if len(instructionList) != 4:
         return instructionString
     
@@ -108,6 +127,21 @@ def addsToBinary(instructionString):
                 imm3 = '0' + imm3
 
         return "0001110 " + imm3 + " " + rn + " " + rd
+
+    # 9.1.4.2 9.1.4.1 ADD (SP plus immediate) : Add Immediate to SP (p. 194)
+    # Checking for SP, SP, #<offset>
+    if instructionList[1] == "sp" and instructionList[2] == "sp" and  instructionList[3].startswith('#'):
+        imm7 = format(int(int(instructionList[3].replace('#', '')) / int(4)), 'b')
+        
+        if len(imm7) > 7:
+            raise MemoryError("imm7 is too big (> 7 bits)")
+        else:
+            imm7Size = len(imm7)
+            for i in range(0, 7 - imm7Size):
+                imm7 = '0' + imm7
+        return "101100000 " + imm7
+    
+    return instructionString
 
     return instructionString
 
@@ -178,6 +212,41 @@ def strToBinary(instructionString):
     return instructionString
 
 
+############################################################
+# LDR
+############################################################
+def ldrToBinary(instructionString):
+    """
+        For a given instruction (string) will return the same instruction with ldr remplaced with its binary value.
+    """
+    instructionList = getInstructionList(instructionString)
+    registryKeyList = registryDic.keys()
+
+    # 9.1.3.2 LDR (immediate) : Load Register (p. 246)
+    if len(instructionList) != 4:
+        return instructionString
+    
+    if instructionList[0] != "ldr":
+        return instructionString
+    
+   
+
+    if instructionList[1] in registryKeyList and instructionList[2] == "[sp" and instructionList[3].startswith("#") and instructionList[3].endswith("]"):
+        
+        rt = instructionList[1]
+        imm8 = format(int(int(instructionList[3].replace('#', '').replace(']', '')) / int(4)), 'b')
+        imm8Size = len(imm8)
+        
+        if imm8Size > 8:
+            raise MemoryError("imm8 is too big (> 8 bits)")
+        else:
+            for i in range(0, 8 - imm8Size):
+                imm8 = '0' + imm8
+
+        return "10011 " + rt + " " + imm8
+    
+    return instructionString
+
 def registryToBinary(instructionString):
     """
         For a given instruction (string) will return the same instruction with registry remplaced with their binary values.
@@ -210,7 +279,10 @@ instructions = [
     "movs r0, #0",
     "movs r1, #1",
     "str r1, [sp, #4]",
-    "adds r1, r1, r2"]
+    "ldr r1, [sp, #8]",
+    "ldr r2, [sp, #4]",
+    "adds r1, r1, r2",
+    "add sp, #12"]
 
 
 for instruction in instructions: 
@@ -218,6 +290,7 @@ for instruction in instructions:
     instruction = subsToBinary(instruction)
     instruction = movToBinary(instruction)
     instruction = strToBinary(instruction)
+    instruction = ldrToBinary(instruction)
 
     instruction = registryToBinary(instruction)
     instruction = instructionToHexadecimal(instruction)
